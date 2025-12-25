5e4a9208640c5396c14ee4a629930ea491dbcd06
Daria Cinca <201923680+DariaCinca44@users.noreply.github.com>
2025-12-26 01:56:38 +0200

    added redux

---
+++ redux/2/src/components/NoteList.jsx
@@ -0,0 +1,33 @@
+import { shallowEqual, useSelector, useDispatch } from 'react-redux'
+import { useState } from 'react'
+
+import { addNote, deleteNote } from '../actions/actions'
+
+const noteListSelector = state => state.list.notes
+
+const NoteList = (props) => {
+  const notes = useSelector(noteListSelector, shallowEqual)
+  const [content, setContent] = useState('')
+
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <h3>List of notes</h3>
+        {
+          notes.map((e, i) => <div key={i}>{e}
+          <button onClick={() => dispatch(deleteNote(i))}>Delete</button>
+          </div>)
+        }
+      </div>
+      <div>
+        <h3>Add a note</h3>
+        <input type='text' placeholder='note content' onChange={(evt) => setContent(evt.target.value)} />
+        <input type='button' value='add' onClick={() => dispatch(addNote(content))} />
+      </div>
+    </div>
+  )
+}
+
+export default NoteList
